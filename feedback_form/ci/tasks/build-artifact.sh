50ed8e72dec4505a97fd67589c4159cd4231c49c
Cameron Stewart <cstewart@pivotal.io>
2017-04-01 15:17:58 -0600

    update build scripts with proper maven commands. thnx mj

--- feedback_form/ci/tasks/build-artifact.sh
+++ feedback_form/ci/tasks/build-artifact.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash
-set -e
 
-# export GRADLE_OPTS=-Dorg.gradle.native=false
-# version=`cat version/number`
-cd git-repo/feedback_form
-mvn clean package
-# echo 'done building'
-# cp build/libs/*.war ../artifact-dir/
+#!/bin/bash
+
+set -e -x
+
+pushd git-repo/feedback_form
+  ./mvnw clean package
+popd
+
+cp git-repo/feedback_form/target/demo-web-app-0.0.1-SNAPSHOT.jar   build-output/.
