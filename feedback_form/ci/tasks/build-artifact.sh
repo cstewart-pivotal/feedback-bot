12fd3ebade7f9b496b701e45b23f5d09a3a099fd
Cameron Stewart <cstewart@pivotal.io>
2017-04-01 13:20:05 -0600

    add concourse pipeline

---
+++ feedback_form/ci/tasks/build-artifact.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+# export GRADLE_OPTS=-Dorg.gradle.native=false
+# version=`cat version/number`
+cd git-repo/feedback_form
+mvn clean package
+echo 'done building'
+# cp build/libs/*.war ../artifact-dir/
