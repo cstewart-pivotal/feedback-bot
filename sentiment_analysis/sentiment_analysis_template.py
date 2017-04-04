8cfb9a5ac1ccc027d664681141553cff971b80db
Cameron Stewart <cstewart@pivotal.io>
2017-04-04 09:48:58 -0400

    update Python app

--- sentiment_analysis/sentiment_analysis_template.py
+++ sentiment_analysis/sentiment_analysis_template.py
@@ -13,28 +13,6 @@ port = int(os.getenv("PORT", 18080))
 def logMsg(args):
     print "[Instance: %s] %s" % (str(os.getenv("CF_INSTANCE_INDEX", 0)), args)
 
-# def print_result(annotations):
-#   score = annotations.sentiment.score
-#   magnitude = annotations.sentiment.magnitude
-#
-#   for index, sentence in enumerate(annotations.sentences):
-#     sentence_sentiment = sentence.sentiment.score
-#     print('Sentence {} has a sentiment score of {}'.format(index, sentence_sentiment))
-#     print('Overall Sentiment: score of {} with magnitude of {}'.format(score, magnitude))
-#     return 0
-#
-#     print('Sentiment: score of {} with magnitude of {}'.format(
-#         score, magnitude))
-#     return 0
-
-# def analyze(client, text):
-  # document = client.document_from_text(text)
-  # annotations = document.annotate_text(include_sentiment=True,
-  #                                      include_syntax=False,
-  #                                      include_entities=False)
-  # # Print the results
-  #print_result(annotations)
-
 
 # Handle JSON
 @app.route('/', methods = ['POST', 'GET'])
