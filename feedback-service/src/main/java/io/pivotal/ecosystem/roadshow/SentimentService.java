f037c0aea39f1a68c550ab2d883aa8e900d82590
Cameron Stewart <cstewart@pivotal.io>
2017-06-02 14:30:22 +0200

    add hystrix

--- feedback-service/src/main/java/io/pivotal/ecosystem/roadshow/SentimentService.java
+++ feedback-service/src/main/java/io/pivotal/ecosystem/roadshow/SentimentService.java
@@ -21,11 +21,25 @@ public class SentimentService {
 		this.restTemplate = rest;
 	}
 
+	@HystrixCommand(fallbackMethod = "skipPythonApp")
 	public String callPythonApp(String url, String text) {
 		String userInput = "{\"request\":\"" + text + "\"}";
 		String jsonResult = restTemplate.postForObject(url, userInput, String.class);
 		System.out.println("jsonResult:" + jsonResult);
 		return jsonResult;
+	}
+	public String skipPythonApp(String url, String text) {
+
+		Resource resource = new ClassPathResource("/fallback_response.json");
+		String content = null;
+		try {
+			content = new String(Files.readAllBytes(Paths.get(resource.getURI())));
+		}
+		catch (IOException e){
+			System.err.println("error reading json file");
+			e.printStackTrace();
+		}
+		return content;
 	}
 }
 
@@ -60,16 +74,3 @@ public class SentimentService {
 
 
 
-//	public String skipPythonApp(String url, String text) {
-//
-//		Resource resource = new ClassPathResource("/fallback_response.json");
-//		String content = null;
-//		try {
-//			content = new String(Files.readAllBytes(Paths.get(resource.getURI())));
-//		}
-//		catch (IOException e){
-//			System.err.println("error reading json file");
-//			e.printStackTrace();
-//		}
-//		return content;
-//	}
