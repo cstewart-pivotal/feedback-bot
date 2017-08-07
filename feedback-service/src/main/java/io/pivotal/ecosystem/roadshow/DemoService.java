617d40b845bdd149e4180c6f89699517022e86b8
Cameron Stewart <cstewart@pivotal.io>
2017-08-07 14:50:54 -0700

    add hystrix functionality

--- feedback-service/src/main/java/io/pivotal/ecosystem/roadshow/DemoService.java
+++ feedback-service/src/main/java/io/pivotal/ecosystem/roadshow/DemoService.java
@@ -56,8 +56,12 @@ public class DemoService
 		result.setMagnitude(magnitude);
 		result.setScore(score);
 
+		if((request.equals("Service Down") && (score == 0.0) && (magnitude == 0.0)){
+			String fallbackResponse = "Thanks for the feedback";
+			result.setResponse(fallbackResponse);
 
-		if((score > 0) && (magnitude >= 0.5)){
+		}
+		else if((score > 0) && (magnitude >= 0.5)){
 			String positiveResponse = "Awesome! Thanks for the great feedback! Keep on rockin'!";
 			result.setResponse(positiveResponse);
 		}
